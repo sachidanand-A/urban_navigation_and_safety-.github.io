2d7c2f40c184a02c49905a57965c9f4b23edac16
01fe23bci119 <01fe23bci119@kletech.ac.in>
2024-12-26 22:20:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// Get the navigation element
 const navigation = document.querySelector(".primary-navigation");
 
+// Get the height of the navigation bar
 const navigationHeight = navigation.offsetHeight;
 
+// Set the scroll-padding CSS variable dynamically
 document.documentElement.style.setProperty(
   "--scroll-padding",
-  navigationHeight + "px"
+  navigationHeight + "px"
 );
+
